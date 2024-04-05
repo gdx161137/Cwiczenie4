243677e6c3b5c932a0819a420c0e436576347100
Jakub Pacler <jakub.pacler@gmail.com>
2024-04-05 21:20:09 +0200

    Add project files.

---
+++ Cwiczenie4/Metody.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium.NET_metody
+{
+    internal class Metody
+    {
+        public static string Odwroc(string linliniaTekstu)
+        {
+            char[] charArray = linliniaTekstu.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+
+    }
+}
---
+++ Cwiczenie4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laboratorium.NET_metody;
+
+namespace Laboratorium.NET_cwiczenie3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lokalizacja = "C:\\aaMag\\test.txt";
+
+            if (File.Exists(lokalizacja))
+            {
+                using (StreamReader sr = new StreamReader(lokalizacja))
+                {
+                    string linliniaTekstu;
+
+                    while ((linliniaTekstu = sr.ReadLine()) != null)
+                    {
+                        string reversedLine = Metody.Odwroc(linliniaTekstu);
+                        Console.WriteLine(reversedLine);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Podany plik nie istnieje.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+
+}
